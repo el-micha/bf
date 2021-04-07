52420b79a78bf6919ff01a49e6b26e56a51a2fb8
Michael Weigelt <michael.weigelt@bluewin.ch>
2021-04-07 22:17:52 +0200

    initial commit

---
+++ bf.hs
@@ -0,0 +1,10 @@
+-- brainfuck interpreter
+
+-- read file, tokenize it, check its well-formedness
+-- this is a list of instructions
+-- needs a pointer, just as the tape needs a pointer
+
+-- datatype tape with pointer with interface:
+-- move pointer, read from pointer, write to pointer
+
+
